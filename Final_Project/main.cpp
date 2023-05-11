974f156db513abc53eeeea818c64860432bc64e8
opp20305 <opp20305@gmail.com>
2023-05-11 16:12:12 +0800

    第十三週程式作業

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -1,14 +1,42 @@
-#include<stdio.h>
-#include<GL/glut.h>
+#include <stdio.h>
+#include <GL/glut.h>
+#include "glm.h"
+GLMmodel * head = NULL;
+GLMmodel * body = NULL;
+GLMmodel * uparmR = NULL;
+GLMmodel * lowerarmR = NULL;
+GLMmodel * armR = NULL;
+int show[5] = {1,0,0,0,0};
 float teapotX=0,teapotY=0;
 FILE * fout = NULL;
 FILE * fin = NULL;
+void keyboard(unsigned char key, int x,int y)
+{
+    if(key=='0')show[0]=!show[0];
+    if(key=='1')show[1]=!show[1];
+    if(key=='2')show[2]=!show[2];
+    if(key=='3')show[3]=!show[3];
+    if(key=='4')show[4]=!show[4];
+    glutPostRedisplay();
+}
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    if(head==NULL)
+    {
+        head = glmReadOBJ("model/head.obj");
+        body = glmReadOBJ("model/body.obj");
+        uparmR = glmReadOBJ("model/uparmR.obj");
+        lowerarmR = glmReadOBJ("model/lowarmR.obj");
+        armR = glmReadOBJ("model/armR.obj");
+    }
     glPushMatrix();
-    glTranslatef(teapotX,teapotY,0);
-    glutSolidTeapot(0.3);
+    glScalef(0.3,0.3,0.3);
+    if(show[0]) glmDraw(body,GLM_MATERIAL);
+    if(show[1])glmDraw(uparmR,GLM_MATERIAL);
+    if(show[2])glmDraw(lowerarmR,GLM_MATERIAL);
+    if(show[3])glmDraw(head,GLM_MATERIAL);
+    if(show[4])glmDraw(armR,GLM_MATERIAL);
     glPopMatrix();
     glutSwapBuffers();
 }
@@ -23,16 +51,16 @@ void mouse(int button,int state,int x,int y)
     }
     display();
 }
-void keyboard(unsigned char key,int x,int y)
-{
-    if(fin==NULL)
-    {
-        fclose(fout);
-        fin = fopen("file4.txt","r");
-    }
-    fscanf(fin,"%f%f",&teapotX,&teapotY);
-    display();
-}
+//void keyboard(unsigned char key,int x,int y)
+//{
+    //if(fin==NULL)
+    //{
+       // fclose(fout);
+       // fin = fopen("file4.txt","r");
+    //}
+    //fscanf(fin,"%f%f",&teapotX,&teapotY);
+   // display();
+//}
 int main(int argc,char** argv)
 {
     glutInit(&argc,argv);
