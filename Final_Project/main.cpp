a97c0e214fee9fccd672cc44df6b24e53775c16c
opp20305 <opp20305@gmail.com>
2023-05-25 16:40:59 +0800

    第十五週程式作業

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -7,7 +7,9 @@ GLMmodel * uparmR = NULL;
 GLMmodel * lowerarmR = NULL;
 GLMmodel * armR = NULL;
 int show[5] = {1,1,0,0,0};
+int ID=4;
 float teapotX=0,teapotY=0;
+float angle[20]={};
 FILE * fout = NULL;
 FILE * fin = NULL;
 void keyboard(unsigned char key, int x,int y)
@@ -37,9 +39,22 @@ void display()
         if(show[0]) glmDraw(body,GLM_MATERIAL);
     glPopMatrix();
         if(show[1])glmDraw(head,GLM_MATERIAL);
+        glPushMatrix();
+        glTranslatef(-1.360000,+0.500000,0);
+        glRotatef(angle[2],0,0,1);
+        glTranslatef(1.360000,-0.500000,0);
+        if(ID==2)glColor3f(1,0,0);
+        else glColor3f(1,1,1);
         if(show[2])glmDraw(uparmR,GLM_MATERIAL);
+        glPushMatrix();
+        glTranslatef(-1.959999,+0.080000,0);
+        glRotatef(angle[2],0,0,1);
+        glTranslatef(1.959999,-0.080000,0);
+        if(ID==3)glColor3f(1,0,0);
+        else glColor3f(1,1,1);
         if(show[3])glmDraw(lowerarmR,GLM_MATERIAL);
         if(show[4])glmDraw(armR,GLM_MATERIAL);
+        glPopMatrix();
     glPopMatrix();
     glutSwapBuffers();
 }
@@ -56,6 +71,8 @@ void motion(int x,int y)
 {
     teapotX+=(x-oldX)/150.0*3;
     teapotY-=(y-oldY)/150.0*3;
+    printf("glTranslatef(%f,%f,0)\n",teapotX,teapotY);
+    angle[ID]+=x-oldX;
     oldX=x;
     oldY=y;
     glutPostRedisplay();
