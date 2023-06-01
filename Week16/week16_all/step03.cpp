e948b23a44574fd15fb045ec30dda470debaa3d3
opp20305 <opp20305@gmail.com>
2023-06-01 15:01:11 +0800

    第十六週程式作業

--- Week16/week16_all/step03.cpp
+++ Week16/week16_all/step03.cpp
@@ -1,32 +1,21 @@
-#include <stdio.h>
 #include <GL/glut.h>
-#include "glm.h"
-GLMmodel * gundam = NULL;
 float teapotX = 0, teapotY = 0, angle = 0, oldX = 0, oldY = 0;
 void mouse(int button, int state, int x, int y) {
     oldX = x;
     oldY = y;
 }
 void motion(int x, int y) {
-    teapotX = (x-150)/150.0;
-    teapotY = (150-y)/150.0;
-    angle += x - oldX;
+    //teapotX = (x-150)/150.0;
+    angle += x - oldX;//teapotY = (150-y)/150.0;
     oldX = x;
     glutPostRedisplay();
-    printf("  glTranslatef( %.2f, %.2f, 0 ); \n", teapotX, teapotY );
 }
 void display() {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-
     glPushMatrix();
-        glColor3f(1,0,0);
-        glScalef(0.04, 0.04, 0.04);
-        glmDraw(gundam, GLM_MATERIAL);
+        glRotatef(angle, 0, 0, 1); //glTranslatef(teapotX, teapotY, 0);
+        glutSolidTeapot( 0.3 );
     glPopMatrix();
-
-    glColor3f(0,1,0);
-    glutSolidTeapot( 0.02 );
-
     glutSwapBuffers();
 }
 
@@ -39,8 +28,6 @@ int main(int argc, char** argv)
     glutDisplayFunc(display);
     glutMotionFunc(motion);
     glutMouseFunc(mouse);
-
-    gundam = glmReadOBJ("model/Gundam.obj");
 
     glutMainLoop();
 }
--- Week16/week16_all/step06.cpp
+++ Week16/week16_all/step06.cpp
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <GL/glut.h>
-#include "glm.h"
-GLMmodel * gundam = NULL;
 float teapotX = 0, teapotY = 0, angle = 0, oldX = 0, oldY = 0;
 void mouse(int button, int state, int x, int y) {
     oldX = x;
@@ -17,16 +15,16 @@ void motion(int x, int y) {
 }
 void display() {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-
     glPushMatrix();
-        glColor3f(1,0,0);
-        glScalef(0.04, 0.04, 0.04);
-        glmDraw(gundam, GLM_MATERIAL);
+        glColor3f(1,1,1);
+        glTranslatef(-0.42, +0.06, 0 );
+        glRotatef(angle, 0, 0, 1);
+        glTranslatef( 0.42, -0.06, 0 );//glTranslatef(teapotX, teapotY, 0);
+        glutSolidTeapot( 0.3 );
     glPopMatrix();
 
     glColor3f(0,1,0);
     glutSolidTeapot( 0.02 );
-
     glutSwapBuffers();
 }
 
@@ -39,8 +37,6 @@ int main(int argc, char** argv)
     glutDisplayFunc(display);
     glutMotionFunc(motion);
     glutMouseFunc(mouse);
-
-    gundam = glmReadOBJ("model/Gundam.obj");
 
     glutMainLoop();
 }
